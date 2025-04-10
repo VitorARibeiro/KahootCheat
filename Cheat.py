577954f90becf1194ff6711d08026c04ba4bdd48
Vitor Ribeiro <129668935+VitorARibeiro@users.noreply.github.com>
2025-04-10 17:23:44 +0100

    Python

---
+++ Cheat.py
@@ -0,0 +1,21 @@
+import requests as req
+# b84f489e-c4a1-4eed-b185-173caada4fd0
+
+def RequestQuestion():
+    GameId = input("Indique o Game-ID")
+    r  = req.get(f"https://play.kahoot.it/rest/kahoots/{GameId}") 
+    return r.json().get("questions")
+
+def Parser(input):
+    FinalDictionary = dict()
+    for QuestionNumber in input:
+        Questions = QuestionNumber.get("question")
+        if Questions is not None:
+            Choices = QuestionNumber.get("choices") 
+            CorrectAnswers = [
+                Option.get("answer") 
+                for Option in Choices 
+                if Option.get("correct") is True
+            ]
+            FinalDictionary[Questions] = CorrectAnswers
+    return FinalDictionary
